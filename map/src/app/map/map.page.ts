539b6b3f8091128236e658a506131b8e03b57462
SheyMV <sheydamirzaie@gmail.com>
2022-09-03 22:35:44 +0430

    loading map

--- map/src/app/map/map.page.ts
+++ map/src/app/map/map.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import * as L from "leaflet";
 
 @Component({
   selector: 'app-map',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map.page.scss'],
 })
 export class MapPage implements OnInit {
-
+  didPageInitiolized=false;
+  private map;
   constructor() { }
 
   ngOnInit() {
+    
+  }
+  ionViewDidEnter() {
+    this.didPageInitiolized=true;
+    setTimeout(() => {
+      this.initMap();
+    }, 1000);  }
+
+  private initMap(): void {
+    this.map = L.map('map').setView([35.689167, 51.388889], 15);
+    const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '© OpenStreetMap'
+
+    })
+    tiles.addTo(this.map)
   }
 
 }
